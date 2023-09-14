1cb7961f0f587ba530caec95905c4bd4c7ad1874
chiemezie <emezie.dike1@gmail.com>
2023-09-14 10:08:04 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,22 +3,19 @@
 /**
  * sum_them_all - adds all the numbers
  * @n: the number of parameters passed
- * Return: the sum of all the numbers
+ * Return: Always 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	int sum;
 	unsigned int i;
 	va_list ap;
 
 	va_start(ap, n);
 
 	if (n == 0)
-	{
-		va_end(ap);
 		return 0;
-	}
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
